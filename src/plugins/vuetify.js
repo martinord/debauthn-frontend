4648ec969aca1a97d68f92253011da7d8425422c
Martiño Rivera Dourado <martino.rivera.dourado@udc.es>
2020-05-22 11:20:21 +0200

    Change theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,18 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        dark: false,
+        themes: {
+            light: {
+              primary: '#455A64',
+              accent: '#5D4037',
+              secondary: '#78909C',
+              success: '#7CB342',
+              info: '#64B5F6',
+              warning: '#FFB300',
+              error: '#F4511E'
+            }
+        }
+      },
 });
